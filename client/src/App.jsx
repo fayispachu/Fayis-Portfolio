ba00605d386463f5d4162d63d29defb59898a0d1
Fayis <fayizpachu217@gmail.com>
2025-08-17 08:12:33 +0530

    Mail context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import { MailProvider } from "./context/mailContext";
+import { MailProvider } from "./context/MailContext";
 
 function App() {
   return (
