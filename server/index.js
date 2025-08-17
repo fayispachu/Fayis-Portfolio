890b359709cb99af96160b69e170636f3a2b151a
Fayis <fayizpachu217@gmail.com>
2025-08-17 07:55:53 +0530

    just rout setup

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://your-app.onrender.com"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
--- server/router/mail.route.js
+++ server/router/mail.route.js
@@ -4,4 +4,12 @@ import { sendEmail } from "../controller/mail.controller.js";
 const mailRouter = express.Router();
 
 mailRouter.post("/send-email", sendEmail);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+  });
+}
+
 export default mailRouter;
