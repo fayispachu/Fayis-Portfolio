1b3ceedb194494934ceafe0314aaa16eb62a99ba
Fayis <fayizpachu217@gmail.com>
2025-08-17 08:35:36 +0530

    index.js path dirname setup

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const __dirname = dirname(__filename);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
   });
 }
