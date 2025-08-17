764a6729ed88aeab20c302662fde5d8116aea972
Fayis <fayizpachu217@gmail.com>
2025-08-17 08:09:27 +0530

    package .json setup

--- server/index.js
+++ server/index.js
@@ -20,6 +20,18 @@ ConnectDataBase();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/mail", mailRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- server/router/mail.route.js
+++ server/router/mail.route.js
@@ -4,12 +4,6 @@ import { sendEmail } from "../controller/mail.controller.js";
 const mailRouter = express.Router();
 
 mailRouter.post("/send-email", sendEmail);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-  });
-}
 
 export default mailRouter;
